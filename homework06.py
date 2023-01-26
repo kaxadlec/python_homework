f66ae9c736d854b5ab6f64132ff4218264c96ea1
kaxadlec <kaxadlec@gmail.com>
2023-01-26 20:27:31 +0900

    #9.4

--- homework06.py
+++ homework06.py
@@ -1,14 +1,11 @@
-# 9.3
-def test(func):
-    def new_function(*arg, **kwangs):
-        print('start')
-        result = func(*arg, **kwangs)
-        print('end')
-        return result
-    return new_function
+# 9.4
+class OopsException(Exception):
+    pass
 
-@test
-def exercise():
-    print("실행")
-exercise()
+# raise OopsException()
 
+
+try:
+    raise OopsException
+except OopsException:
+    print('Caught an oops')
